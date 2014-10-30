8d2d918b9e4d90f151c0aca351f3a7c72a4801b3
Matt DeWolfe <matthewdewolfe@gmail.com>
2014-10-30 11:32:52 -0400

    Added components to ECS system

--- TheForge/TheForge/include/ECS/GameEntity.h
+++ TheForge/TheForge/include/ECS/GameEntity.h
@@ -9,15 +9,40 @@
 typedef std::map<GameComponentType, IGameComponentPtr> GameComponentMap;
 
 class GameEntity
+
 {
 public:
 	GameEntity();
 	~GameEntity();
 
-protected:/*
-	Vector3 _position;
-	Vector3 _rotation;
-	Vector3 _velocity;*/
+	// Should iteratre through all components and update
+	void Update(float _deltaTime);
+
+	// Add a component to this entity
+	void AddComponent(IGameComponent* _component);
+	// Attempt to remove a component by its component type
+	bool RemoveComponentByType(GameComponentType _type);
+	// Attempt to remove a component by its game ID
+	bool RemoveComponentByID(int _ID);
+	// Attempt to remove a specific component
+	bool RemoveComponent(IGameComponent* _component);
+	// Free memory held by components
+	void Shutdown();
+
+	// Getters and setters
+	void SetPosition(float x, float y, float z);
+	float3 GetPosition();
+
+	void SetRotation(float x, float y, float z);
+	float3 GetRotation();
+
+	void SetVelocity(float x, float y, float z);
+	float3 GetVelocity();
+
+protected:
+	float3 _position;
+	float3 _rotation;
+	float3 _velocity;
 };
 
 #endif
---
+++ TheForge/TheForge/include/ECS/RigidBodyComponent.h
@@ -0,0 +1,10 @@
+#ifndef RIGIDBODYCOMPONENT_H
+#define RIGIDBODYCOMPONENT_H
+
+#include "IGameComponent.h"
+
+class RigidBodyComponent : public IGameComponent
+{
+
+};
+#endif
