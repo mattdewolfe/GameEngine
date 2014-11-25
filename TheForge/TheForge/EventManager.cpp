5e45f5c7094051b5c3c78445f9d2a71be5b25c6b
Matt DeWolfe <matthewdewolfe@gmail.com>
2014-11-25 08:37:40 -0500

    Added function definitions to eventmanager.cpp

--- TheForge/TheForge/EventManager.cpp
+++ TheForge/TheForge/EventManager.cpp
@@ -27,17 +27,60 @@ bool EventManager::VAddListener(const EventListenerDelegate& _eventDelegate,
 bool EventManager::VARemoveListener(const EventListenerDelegate& _eventDelegate, 
 		const EventType& _type)
 {
-
+	bool success = false;
+	// Search through listeners for the specified event type
+	auto findIt = eventListeners.find(_type);
+	if (findIt != eventListeners.end())
+	{
+		EventListenerList& listeners = findIt->second;
+		// Move through listeners to find target delegate
+		for (auto it = listeners.begin(); it != listeners.end(); it++)
+		{
+			if (_eventDelegate == (*it))
+			{
+				listeners.erase(it);
+				success = true;
+				// We can break out now since the same delegate should not
+				// be registered to the same event type more than once
+				break;
+			}
+		}
+	}
+	return success;
 }
 	
 bool EventManager::VTriggerEvent(const IEventDataPtr& _ptrEvent)
 {
-
+	bool processed = false;
+	// Find the target event 
+	auto findIt = eventListeners.find(_ptrEvent->VGetEventType());
+	if (findIt != eventListeners.end())
+	{
+		const EventListenerList& eventListenerList = findIt->second;
+		// Move through event listener list to find event
+		for (EventListenerList::const_iterator it = eventListenerList.begin();
+			it != eventListenerList.end(); it++)
+		{
+			EventListenerDelegate listener = (*it);
+			listener(_ptrEvent);
+			processed = true;
+		}
+	}
+	return processed;
 }
 	
 bool EventManager::VQueueEvent(const IEventDataPtr& _ptrEvent)
 {
-
+	auto findIt = eventListeners.find(_ptrEvent->VGetEventType());
+	if (findIt != eventListeners.end())
+	{
+		queues[activeQueue].push_back(_ptrEvent);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 bool EventManager::VTerminateAll(const EventType& _type)
