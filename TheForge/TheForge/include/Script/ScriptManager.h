22d8e1d81b2b1ad7f22837e0ddcfe835b9213f18
Matt DeWolfe <matthewdewolfe@gmail.com>
2014-12-06 11:39:09 -0500

    Basic value loading implemented

--- TheForge/TheForge/include/Script/ScriptManager.h
+++ TheForge/TheForge/include/Script/ScriptManager.h
@@ -2,13 +2,22 @@
 #define SCRIPTMANAGER_H
 
 #include <assert.h>
+#include <Windows.h>
 #include <iostream>
-#include <fstream>
+#include <sstream> 
 #include <string>
 #include "rapidxml.hpp"
 #include "rapidxml_iterators.hpp"
 #include "rapidxml_print.hpp"
 #include "rapidxml_utils.hpp"
+
+// Macro for tracing event data names into output window
+#define DBOUT( s )            \
+{                             \
+   std::wostringstream os_;    \
+   os_ << s << "\n";                   \
+   OutputDebugStringW( os_.str().c_str() );  \
+}
 
 class ScriptManager
 {
@@ -22,10 +31,12 @@ public:
 	bool LoadScript(const char* _fileName);
 	
 	// Set a variable by passing in the variable name in script and the variable itself
-	void GetVariableFromScript(char* _name, float *var);
-	void GetVariableFromScript(char* _name, int *var);
-	void GetVariableFromScript(char* _name, bool *var);
-	void GetVariableFromScript(char* _name, char *var);
+	// The variable must be passed in to determine type to return
+	// @Parem - node to look under, attribute name, variable type to return
+	float GetVariableFromScript(char* _node, char* _name, float *var);
+	int GetVariableFromScript(char* _node, char* _name, int *var);
+	bool GetVariableFromScript(char* _node, char* _name, bool *var);
+	std::string GetVariableFromScript(char* _node, char* _name, std::string *var);
 	/*
 	void CreateComponentFromScript(char* _name, ICompoment* comp);
 	float SetVariableInScript(char* _name, float &var);
--- TheForge/TheForge/src/Script/ScriptManager.cpp
+++ TheForge/TheForge/src/Script/ScriptManager.cpp
@@ -2,45 +2,72 @@
 
 ScriptManager::ScriptManager()
 {
+
 }
 
-void ScriptManager::GetVariableFromScript(char* _name, float* var)
+float ScriptManager::GetVariableFromScript(char* _node, char* _name, float* var)
 {
-	rapidxml::xml_node<> *node = doc.first_node(_name);
-	char* ret = node->value();
-	float total;
+	rapidxml::xml_node<>* parent = doc.first_node();
+	rapidxml::xml_node<>* targetNode = parent->first_node(_name);
+	char* ret = targetNode->value();
+	float total = 0.0f;
+	bool afterDecimal = false;
 	for (int i = 0; ret[i] != '\0'; i++)
 	{
-		total = (float)(ret[i] - '0');
+		if (ret[i] == '.')
+		{
+			// If we find a second decimal place, break and ignore further values
+			if (afterDecimal == true)
+			{
+				DBOUT("SCRIPT: Read float: Terminated early due to second decimal.");
+				break;
+			}
+			afterDecimal = true;
+		}
+		else
+		{
+			if (afterDecimal == false)
+				total += (float)(ret[i] - '0');
+			else
+				total += (float)(ret[i] - '0')/10;
+		}
 	}
-	var = &total;
+	DBOUT("SCRIPT: Read float: " << total);
+	return total;
 }
 
-void ScriptManager::GetVariableFromScript(char* _name, int* var)
+int ScriptManager::GetVariableFromScript(char* _node, char* _name, int* var)
 {
-	rapidxml::xml_node<> *node = doc.first_node(_name);
-	int ret = (int)node->value();
-	var = &ret;
+	rapidxml::xml_node<>* parent = doc.first_node();
+	rapidxml::xml_node<>* targetNode = parent->first_node(_name);
+	int ret = (int)targetNode->value();
+	DBOUT("SCRIPT: Read int: " << targetNode->value());
+	return ret;
 }
 
-void ScriptManager::GetVariableFromScript(char* _name, bool* var)
+bool ScriptManager::GetVariableFromScript(char* _node, char* _name, bool* var)
 {
-	rapidxml::xml_node<> *node = doc.first_node(_name);
-	if (node->value() == "true")
+	rapidxml::xml_node<>* parent = doc.first_node();
+	rapidxml::xml_node<>* targetNode = parent->first_node(_name);
+	DBOUT("SCRIPT: Read bool: " << targetNode->value());
+	if (targetNode->value() == "true")
 	{ 
-		*var = true;
+		
+		return true;
 	}
 	else
 	{
-		*var = false;
+		return false;
 	}
 }
 
-void ScriptManager::GetVariableFromScript(char* _name, char *var)
+std::string ScriptManager::GetVariableFromScript(char* _node, char* _name, std::string* var)
 {
-	rapidxml::xml_node<> *node = doc.first_node(_name);
-	char* ret = node->value();
-	var = ret;
+	rapidxml::xml_node<>* parent = doc.first_node();
+	rapidxml::xml_node<>* targetNode = parent->first_node(_name);
+	std::string ret = targetNode->value();
+	DBOUT("SCRIPT: Read string: " << targetNode->value());
+	return ret;
 }
 
 bool ScriptManager::Init()
@@ -55,8 +82,8 @@ bool ScriptManager::LoadScript(const char* _fileName)
 	if (xmlFile != nullptr)
 	{
 		doc.parse<0>(xmlFile->data());
-		return true;
 	}
+	DBOUT( "SCRIPT: Loaded " << doc.first_node()->name() << " script." );
 	return false;
 }
 
@@ -67,7 +94,10 @@ void ScriptManager::Update(float _dt)
 
 void ScriptManager::Shutdown()
 {
+	doc.clear();
 
+	delete xmlFile;
+	xmlFile = nullptr;
 }
 
 // Global print function for string output
